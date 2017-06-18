926d34ae8f327417a0b732c4747fa17f7e043c34
Zach Newton <znewton@iastate.edu>
2017-06-18 09:15:02 -0500

    Accessed youtube and searched channels

--- controller.js
+++ controller.js
@@ -1,23 +1,56 @@
+const https = require('https');
+const querystring = require('query-string');
+const API_KEY = 'AIzaSyBB0cENZdGYDoSn1H0cRmwJ-KsjdIMaWAA';
+
 /// Searches YouTube for a list of channels matching
 /// a search term.
-exports.get_channels_by_search = function (req, res) {
+exports.get_channels_by_search = function (request, response) {
   console.log('Received request for channel search:');
-  console.log(req.query);
-  res.json({
-    channels: [1, 2, 3]
+  console.log(request.query);
+  const params = {
+    key: API_KEY,
+    part: 'snippet',
+    type: 'channel',
+    q: request.query.q,
+    maxResults: 25
+  }
+  const options = {
+    hostname: 'www.googleapis.com',
+    port: 443,
+    path: `/youtube/v3/search?${querystring.stringify(params)}`,
+    method: 'GET'
+  };
+
+  const req = https.request(options, (res) => {
+    let data = ''
+    res.setEncoding('utf8');
+    res.on('data', (d) => {
+      data += d
+    });
+    res.on('end', () => {
+      const parsedData = JSON.parse(data)
+      response.json(parsedData.items)
+    });
   });
+
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  req.end();
 }
 
 /// Gets the playlists of a given YouTube channel.
-exports.get_channel_playlists = function (req, res) {
+exports.get_channel_playlists = function (request, response) {
   console.log('Received request for channel\'s playlists:');
-  console.log(req);
-  res.json(req);
+  console.log(request.params);
+  response.json({
+    result: 'success!'
+  });
 }
 
 /// Combines the given playlists into a mix.
-exports.get_mix = function (req, res) {
+exports.get_mix = function (request, response) {
   console.log('Received request for mix:');
-  console.log(req);
-  res.json(req);
+  console.log(request);
+  response.json(request);
 }
