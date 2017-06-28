a88038cb5b94db82be313799dd4786f237a10862
Zach Newton <znewton@iastate.edu>
2017-06-28 17:23:22 -0500

    access allow headers

--- routes.js
+++ routes.js
@@ -6,6 +6,18 @@ module.exports = function(app) {
   // serve static assets normally
   app.use(express.static(__dirname + '/'));
 
+  app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    next();
+  })
   // Routes
   app.route('/')
     .get((req, res) => {
